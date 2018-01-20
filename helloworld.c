2ff97b05fd102866932c7ac07957b70259d465f3
Jack Henry Dempsey <jdempsey@cse.unl.edu>
2018-01-20 17:22:43 -0600

    Assignment 1 GitHub

---
+++ helloworld.c
@@ -0,0 +1,16 @@
+/**
+*Author: Jack Dempsey
+*Date: 1/20/2018
+*
+*This program prints my name 'Jack Dempsey" message
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv){
+
+printf("Jack Dempsey!\n");
+
+
+return 0;
+}
